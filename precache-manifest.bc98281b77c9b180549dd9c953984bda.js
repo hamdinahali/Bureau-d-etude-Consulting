bc8fc283d2118b1d2434d45aae4ae57180ca2a3b
Macbook pro <macbookpro@MacBook-Pro-de-Macbook.local>
2024-09-23 17:24:15 +0100

    Updates

---
+++ precache-manifest.bc98281b77c9b180549dd9c953984bda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2d38035d429c6b993c141d384d5349",
+    "url": "/hamdinahali/Bureau-d-etude-Consulting/index.html"
+  },
+  {
+    "revision": "d7c69206610c89405a4e",
+    "url": "/hamdinahali/Bureau-d-etude-Consulting/static/css/main.08ea875f.chunk.css"
+  },
+  {
+    "revision": "70624b60af0fafa0f880",
+    "url": "/hamdinahali/Bureau-d-etude-Consulting/static/js/2.c9a995d6.chunk.js"
+  },
+  {
+    "revision": "1876019e6f8cf7ca83e2",
+    "url": "/hamdinahali/Bureau-d-etude-Consulting/static/js/3.0e4cdb98.chunk.js"
+  },
+  {
+    "revision": "d7c69206610c89405a4e",
+    "url": "/hamdinahali/Bureau-d-etude-Consulting/static/js/main.54152de5.chunk.js"
+  },
+  {
+    "revision": "5dd6053c049e8f4d3f61",
+    "url": "/hamdinahali/Bureau-d-etude-Consulting/static/js/runtime~main.2d258e08.js"
+  },
+  {
+    "revision": "672f699f7ad0f16deed71f904f08dae0",
+    "url": "/hamdinahali/Bureau-d-etude-Consulting/static/media/hh.672f699f.webp"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/hamdinahali/Bureau-d-etude-Consulting/precache-manifest.bc98281b77c9b180549dd9c953984bda.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hamdinahali/Bureau-d-etude-Consulting/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
